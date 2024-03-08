290c13085a48cb07c629e6807c487bb79e4976e6
Paul-Simpson <paul@realisestudio.com>
2024-03-08 00:01:30 +0000

    t2

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -80,7 +80,7 @@ export const config: TConfig = {
     works: {
       p: "My work",
       h2: "Projects.",
-      content: `Following projects showcases my skills and experience through
+      content: `!Following projects showcases my skills and experience through
     real-world examples of my work. Each project is briefly described with
     links to code repositories and live demos in it. It reflects my
     ability to solve complex problems, work with different technologies,
